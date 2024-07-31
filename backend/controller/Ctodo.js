0ad7ceada21de785628339a7f11807b6a20ad667
aixion1506 <aixion1506@gmail.com>
2024-07-31 18:25:35 +0900

    특정 아이디 조회,수정 정상,에러메세지

--- backend/controller/Ctodo.js
+++ backend/controller/Ctodo.js
@@ -34,9 +34,59 @@ exports.getTodoId = async (req, res) => {
         const Todo = await todo.findOne({
             where: { id },
           }); 
+          if (!Todo) return res.json({'message': 'Todo not found'});
           res.json(Todo);
     }catch(err) {
         console.error(err);
         res.status(500).send('Internal Server Error');
+    }
+}
+
+
+// 특정 ID의 Todo 수정
+exports.patchTodoId = async (req, res) => {
+    try {
+        console.log(req.params.id);
+        const { id } = req.params;
+        const { done } = req.body;
+
+        const updatedTodo = await todo.update(
+            // 무엇을 바꾸는지?
+            { done },
+            // 누구를 바꾸는지?
+            { where: {id}}
+        );
+
+        const Todo = await todo.findOne({
+            where: { id },
+          }); 
+          if (!Todo) return res.json({'message': 'Todo not found'});
+          res.json(Todo);
+        // if (!updatedTodo) return res.status(500).send('message: Todo not found');
+        // res.json(updatedTodo);
+
+      
+    } catch (err) {
+        console.log("message: Todo not found")
+        res.status(500).send('Internal Server Error');
+    }
+    
+}
+
+// 특정 ID의 Todo 삭제
+exports.deleteTodoId =  async (req, res) => {
+    try {
+        const { id } =req.params;
+        const isDeleted = await Player.destroy({
+            where: {id}
+        });
+        console.log(isDeleted); // 1
+
+        if(isDeleted) return res.send(true);
+        else return res.send(false);
+    } catch (err) {
+        console.log("message: Todo not found")
+        // res.status(500).send('message: Todo not found');
+        // res.status(500).send('Internal Server Error');
     }
 }
--- backend/routes/todo.js
+++ backend/routes/todo.js
@@ -5,5 +5,7 @@ const controller = require('../controller/Ctodo');
 router.post('/', controller.postTodo);;
 router.get('/', controller.getTodos)
 router.get('/:id', controller.getTodoId);
+router.patch('/:id', controller.patchTodoId);
+router.delete('/:id', controller.deleteTodoId);
 
 module.exports = router;
